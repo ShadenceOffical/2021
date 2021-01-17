24ea0a23bb9cce2cd021eca78410ac11bdf2ced2
ShadenceOffical <62353878+ShadenceOffical@users.noreply.github.com>
2021-01-17 17:17:01 +0700

    Add files via upload

---
+++ htaccess/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>mok415</title>
+</head>
+<body>
+<?php
+	echo 'index<br>';
+	var_dump($_SERVER['REQUEST_URI']);
+	echo '<br>';
+	var_dump($_GET['get']);
+?>
+</body>
+</html>
---
+++ htaccess/test.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>mok415</title>
+</head>
+<body>
+<?php
+	echo 'test<br>';
+	var_dump($_SERVER['REQUEST_URI']);
+?>
+</body>
+</html>
